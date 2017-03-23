21c177cacec874511408602cbb33399113585b7d
priyanka311089 <kesharwanipriyanka31@gmail.com>
2017-03-24 04:17:15 +0530

    final version

--- assets/js/taskManagement.js
+++ assets/js/taskManagement.js
@@ -11,29 +11,32 @@ angular
   function($scope, $http, $rootScope, windowObj) {
     $scope.showInputSection=false;
     $scope.taskList=[];
-
-    $scope.updateTasks = function(){
+    $scope.index = 0;
+   //  $scope.updateTasks();
+    $scope.updateTasks=function(){
       $http.get('/api/getTasks').success(function(response) {
+            if(response.length>0){
+              $scope.index = parseInt(response[response.length-1].taskId.split("_")[1])+1;
 
-          $scope.taskList = response;
-      });
-    }
-    $scope.updateTasks();
-    $scope.updateBoard=function(){
-      $http.get('/api/getTasks').success(function(response) {
+            }
             $scope.taskList = response;
             console.log(response);
-      });
-    };
-    $scope.saveTask = function(){
 
-            var task = {
+      });
+
+    };
+  $scope.updateTasks();
+    $scope.saveTask = function(){
+          var task = {
+              taskId : "task_"+$scope.index,
               title : $scope.titleList,
               cardList:[]
             }
+            $scope.index++;
             $http.post('/api/addTask', task)
                 .success(function(response) {
                     if (response) {
+
                       $scope.taskList = response;
                       console.log("task added in board");
                       $scope.showInputSection = false;
@@ -41,7 +44,9 @@ angular
             });
 
     };
-    $scope.addTaskList = function(){
+    $scope.addTaskList = function($event){
+          $(".input-section").hide();
+          $($event.target).parent().find(".add-task-section").show();
           $scope.titleList = "";
           $scope.showInputSection=true;
     };
@@ -54,32 +59,66 @@ angular
     return {
         restrict: 'E',
         scope: {
-          taskTitle: '@'
+          task: '='
         },
          templateUrl: 'assets/js/views/task.html',
          controller: ['$scope', '$http',function MyTabsController($scope,$http) {
            $scope.showInputSection=false;
-           $scope.id = 0;
-              $http.get('/api/getCardList/'+$scope.taskTitle).success(function(response) {
-                $scope.id = response.length
-                $scope.cardList = response;
+           $scope.index = 0;
+           $scope.getCardList = function(){
+              $http.get('/api/getCardList/'+$scope.task.taskId).success(function(response) {
+                if(response.length>0){
+                $scope.index = parseInt(response[response.length-1].cardId.split("_")[3])+1;
+
+              }
+
+              $scope.cardList = response;
             });
+          };
+          $scope.showEditTask=function(){
+            $scope.editTaskTitle = true;
+
+          }
+          $scope.hideEditTask=function(){
+            $scope.editTaskTitle = false;
+
+          }
+          $scope.editTask=function(){
+            $http.post('/api/editTask',{ newTaskTitle:$scope.newTaskTitle ,oldTask:$scope.task}).success(function(response) {
+                 $scope.$parent.$parent.updateTasks();
+                 $scope.editTaskTitle = false;
+             });
+          }
+
+          $scope.removeTask= function(){
+            $http.post('/api/removeTask',$scope.task).success(function(response) {
+                //Update card list
+                $scope.$parent.$parent.updateTasks();
+             });
+          }
+          $scope.getCardList();
           $scope.addCard = function(){
+
                 var card = {
-                    cardId : $scope.taskTitle+$scope.id,
-                    detail : $scope.cardDetail,
-                    taskTitle : $scope.taskTitle
+                    cardId : $scope.task.taskId+"_card_"+$scope.index,
+                    cardDetail : $scope.cardDetail,
+                    member : "",
+                    taskId : $scope.task.taskId
                   }
+
                   $http.post('/api/addCard', card)
                       .success(function(response) {
                           if (response) {
+                            $scope.index = parseInt(response.cardList[response.cardList.length-1].cardId.split("_")[3])+1;
                             $scope.cardList = response.cardList;
                             console.log("card added in board");
                             $scope.showInputSection = false;
                           }
                   });
           };
-          $scope.addCardList = function(){
+          $scope.addCardList = function($event){
+                $(".input-section").hide();
+                $($event.target).parent().parent().parent().find(".add-card-section").show();
                 $scope.cardDetail = "";
                 $scope.showInputSection=true;
           };
@@ -102,36 +141,79 @@ angular
           $scope.showEditingSection = false;
            $scope.taskList = [];
           $scope.editOptions = {
-            editLabelBox :false,
+            editDetailBox :false,
             editMemberBox :false,
             selectTaskBox : false
           };
-          $http.get('/api/getTasks').success(function(response) {
-            $scope.taskList = response;
-            $scope.selectedTask = response[0];
-              //$scope.$apply($scope.taskList);
-          });
-          $scope.editCard=function(card){
+          $scope.updateCardList=function(){
+            $http.get('/api/getTasks').success(function(response) {
+              $scope.taskList = response;
+              $scope.selectedTask = response[0];
+                //$scope.$apply($scope.taskList);
+            });
+          };
+          $scope.updateCardList();
+
+          $scope.editCard=function($event){
+            $(".input-section").hide();
+            $($event.target).parent().parent().parent().find(".edit-card-section").show();
             $scope.showEditingSection = true;
           }
           $scope.closeEditCard =function(){
               $scope.showEditingSection = false;
+              $scope.editOptions.selectTaskBox = false;
+              $scope.editOptions.editMemberBox = false;
+              $scope.editOptions.editDetailBox = false;
           }
           $scope.showMoveCard=function(){
            $scope.editOptions.selectTaskBox = true;
            $scope.editOptions.editMemberBox = false;
-           $scope.editOptions.editLabelBox = false;
+           $scope.editOptions.editDetailBox = false;
          }
-         $scope.moveCard=function(card){
-
-           var moveCardData ={
-             card:card,
-             newTask : $scope.selectedTask
-           }
-           $http.post('/api/moveCard',moveCardData).success(function(response) {
-               $scope.cardList = response;
-               $scope.$parent.$parent.$parent.$parent.updateTasks();
+         $scope.showCardMemberBox=function(){
+          $scope.editOptions.selectTaskBox = false;
+          $scope.editOptions.editMemberBox = true;
+          $scope.editOptions.editDetailBox = false;
+        }
+        $scope.showCardDetailBox=function(){
+         $scope.editOptions.selectTaskBox = false;
+         $scope.editOptions.editMemberBox = false;
+         $scope.editOptions.editDetailBox = true;
+       }
+         $scope.removeCard= function(card){
+           $http.post('/api/removeCard',card).success(function(response) {
+               //Update card list
+               $scope.$parent.$parent.getCardList();
             });
+         }
+         $scope.submitCard=function(card){
+           if($scope.editOptions.selectTaskBox){
+             var moveCardData = {
+               card:card,
+               newTask : $scope.selectedTask
+             }
+             $http.post('/api/moveCard',moveCardData).success(function(response) {
+                 //Update whole board
+                 $scope.$parent.$parent.$parent.$parent.updateTasks();
+              });
+           }
+           else if($scope.editOptions.editMemberBox){
+             var changedCard = card;
+             card.member = $scope.newCardMember
+             $http.post('/api/ChangeCardData',card).success(function(response) {
+                //Update whole board
+                 $scope.$parent.$parent.$parent.$parent.updateTasks();
+              });
+           }
+           else if($scope.editOptions.editDetailBox){
+             var changedCard = card;
+             card.cardDetail = $scope.newCardDetail;
+             $http.post('/api/ChangeCardData',card).success(function(response) {
+                 console.log(response);
+                 $scope.$parent.$parent.$parent.$parent.updateTasks();
+              });
+           }
+
           }
 
         } //DOM manipulation
--- server.js
+++ server.js
@@ -29,10 +29,10 @@ var server = app.listen(8080, function() {
     var host = server.address().address,
         port = server.address().port;
 
-    //console.log('taskManager started at ' + port);
+    console.log('Tasko.com started at ' + port);
 })
 
-//returns all types of products
+//returns all types of tasks
 app.get('/api/getTasks', function(req, res) {
    jsonfile.readFile(file, function(err, obj) {
      if(taskManagerData.length == 0){
@@ -40,9 +40,9 @@ app.get('/api/getTasks', function(req, res) {
           res.json(obj.tasks);
       }
       else{
-        //console.log("taskManagerData",taskManagerData);
+        console.log("taskManagerData",taskManagerData);
         res.json(taskManagerData)
-        res.status(200);
+
       }
 
   })
@@ -51,14 +51,14 @@ app.get('/api/getTasks', function(req, res) {
 
 
 //returns a product in the cart
-app.get('/api/getCardList/:taskTitle', function(req, res) {
+app.get('/api/getCardList/:taskId', function(req, res) {
 
   for (var i = 0; i < taskManagerData.length; i++) {
       //console.log("Record entered in loop");
           //console.log(taskManagerData[i].title);
           //console.log( req.params.taskTitle);
 
-      if (taskManagerData[i].title == req.params.taskTitle){
+      if (taskManagerData[i].taskId == req.params.taskId){
         //console.log("list--",taskManagerData[i].cardList);
         res.json(taskManagerData[i].cardList);
         res.status(200);
@@ -66,10 +66,22 @@ app.get('/api/getCardList/:taskTitle', function(req, res) {
       }
     }
 });
-app.post('/api/moveCard', function(req, res) {
-  var card=req.body.card;
+app.post('/api/removeTask', function(req, res) {
+  var task=req.body;
+  console.log(task);
    for (var i = 0; i < taskManagerData.length; i++) {
-    if (taskManagerData[i].title == card.taskTitle){
+      if (taskManagerData[i].taskId == task.taskId){
+            taskManagerData.splice(i,1);
+            console.log("after:",taskManagerData);
+      }
+    }
+     res.json("{message:'success'}");
+     res.status(200);
+});
+app.post('/api/removeCard', function(req, res) {
+  var card=req.body;
+   for (var i = 0; i < taskManagerData.length; i++) {
+    if (taskManagerData[i].taskId == card.taskId){
         for (var j = 0; j < taskManagerData[i].cardList.length; j++) {
           if (taskManagerData[i].cardList[j].cardId == card.cardId){
                 var cardList=taskManagerData[i].cardList;
@@ -81,54 +93,56 @@ app.post('/api/moveCard', function(req, res) {
         }
       }
      }
-   card.taskTitle=req.body.newTask.title;
+     res.json("{message:'success'}");
+     res.status(200);
+   });
+   //Get tasks which are in cart
+   app.post('/api/ChangeCardData', function(req, res) {
+     var card=req.body;
+      for (var i = 0; i < taskManagerData.length; i++) {
+       if (taskManagerData[i].taskId == card.taskId){
+           for (var j = 0; j < taskManagerData[i].cardList.length; j++) {
+             if (taskManagerData[i].cardList[j].cardId == card.cardId){
+                    taskManagerData[i].cardList[j].cardDetail = card.cardDetail;
+                    taskManagerData[i].cardList[j].member = card.member;
+                    console.log("updated cards:",taskManagerData[i].cardList[j]);
+
+             }
+           }
+         }
+        }
+        res.json("{message:'success'}");
+        res.status(200);
+   });
+
+app.post('/api/moveCard', function(req, res) {
+  var card=req.body.card;
+   for (var i = 0; i < taskManagerData.length; i++) {
+    if (taskManagerData[i].taskId == card.taskId){
+        for (var j = 0; j < taskManagerData[i].cardList.length; j++) {
+          if (taskManagerData[i].cardList[j].cardId == card.cardId){
+                var cardList=taskManagerData[i].cardList;
+                console.log("before:",taskManagerData,cardList);
+                cardList.splice(j,1);
+                  console.log("after:",taskManagerData);
+
+          }
+        }
+      }
+     }
+   card.taskId=req.body.newTask.taskId;
     for (var i = 0; i < taskManagerData.length; i++) {
-            if  (taskManagerData[i].title == req.body.newTask.title){
+            if  (taskManagerData[i].taskId == req.body.newTask.taskId){
 
                 taskManagerData[i].cardList.push(card)
           }
     }
         console.log("updated:",taskManagerData);
-    res.json(taskManagerData);
-    res.status(200);
+        res.json("{message:'success'}");
+        res.status(200);
 
 });
-//Adds a task
-app.post('/api/addTask', function(req, res) {
 
-    var resJSON = req.body;
-
-
-      //console.log(taskManagerData);
-        var updatedQty = null;
-        tasksPageLen = taskManagerData.length,
-            foundObject = null;
-
-        for (var i = 0; i < tasksPageLen; i++) {
-            //console.log("Record entered in loop");
-
-            if (taskManagerData[i].title == req.body.title){
-                //console.log("Same Record Found");
-                foundObject = taskManagerData[i];
-                taskManagerData.splice(i,1);
-                taskManagerData.push(resJSON)
-                res.json(taskManagerData);
-
-                //console.log("Record updated - 1st Time");
-            }
-        }
-
-        if (!foundObject) {
-            //console.log("Record Added - 2nd Time", taskManagerData.length);
-            taskManagerData.push(resJSON);
-
-            //console.log(taskManagerData);
-            // res.status(200);
-            res.json(taskManagerData);
-        }
-
-
-});
 //Adds card in task
 app.post('/api/addCard', function(req, res) {
 
@@ -143,55 +157,35 @@ app.post('/api/addCard', function(req, res) {
         for (var i = 0; i < tasksPageLen; i++) {
             //console.log("Record entered in loop");
 
-            if (taskManagerData[i].title == req.body.taskTitle){
+            if (taskManagerData[i].taskId == req.body.taskId){
                 //console.log("Same Record Found");
                 foundObject = taskManagerData[i];
                 taskManagerData[i].cardList.push(resJSON)
                 res.json(taskManagerData[i]);
-
+                break;
                 //console.log("Card Added - 1st Time");
             }
         }
 
+
 });
-//Removes a product from the cart
-app.post('/api/removeFromCart', function(req, res) {
+app.post('/api/addTask', function(req, res) {
+         var resJSON = req.body;
+          taskManagerData.push(resJSON);
+         res.json(taskManagerData);
+});//Adds card in task
 
-    var resJSON = req.body;
 
-    if (taskManagerData.length > 0) {
-
-        var updatedQty = null;
-        tasksPageLen = taskManagerData.length,
-        foundObject = null;
-
-        for (var i = 0; i < tasksPageLen; i++) {
-           if (taskManagerData[i].p_id == req.body.p_id && taskManagerData[i].p_selected_size.code == req.body.p_size && taskManagerData[i].p_quantity == req.body.p_quantity && taskManagerData[i].p_selected_color.name == req.body.p_selectedColor) {
-            taskManagerData.splice(i, 1);
-            break;
+//Adds card in task
+app.post('/api/editTask', function(req, res) {
+   for (var i = 0; i < tasksPageLen; i++) {
+            if (taskManagerData[i].taskId == req.body.oldTask.taskId){
+                taskManagerData[i].title = req.body.newTaskTitle;
+                console.log("changed task")
+                break;
             }
-        }
-        //console.log("xxxx="+taskManagerData);
+          }
+
+        res.status(200);
         res.json(taskManagerData);
-
-
-        res.end();
-    }
 });
-
-
-//Get products which are in cart
-app.get('/api/cartPage', function(req, res) {
-  if(taskManagerData.length == 0){
-    jsonfile.readFile(file, function(err, obj) {
-        taskManagerData = obj.tasks;
-        //console.log("DATA" , taskManagerData);
-        res.json(taskManagerData);
-    })
-  }else{
-    //console.log(taskManagerData);
-    res.json(taskManagerData)
-    res.status(200);
-  }
-
-})
